691edf7287a7ddb6c9564567521750fd12d86f90
mikeill <mike@mzoo.org>
2022-08-29 13:13:34 -0500

    Default with no background color, black text.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'ccd962e5a342a7ee05de');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b96a68a2c00b468b36cb');
